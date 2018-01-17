bccc7dd66edfc5d8bb1f16eef36c08398314867a
Neoharry Michailidis <neoharry10@outlook.com.gr>
2018-01-17 19:02:09 +0200

    Polished Mechanics

--- Characters/Neo/Neo.gd
+++ Characters/Neo/Neo.gd
@@ -6,41 +6,52 @@ var Movement
 var Jump = true
 var Offset
 var RelativeMousePos
+var WeaponACanEquip = false
+var WeaponBCanEquip = false
+var WeaponAEquipped = false
+var WeaponBEquipped = false
+
 
 ###Character Specific Variables
 var Speed = 1
 var EndSpeed = 3
 var Gravity = 0.2
-var hid = true
 var JumpAntiForce = 0
-
 
 func _ready():
 	set_fixed_process(true)
-	for i in get_tree().get_nodes_in_group("Pickup"):
-		connect("PickupWeapon", i, "Pick")
+	
 
 	
 	
 func _fixed_process(delta):
-	##Testing the weapons
-	if Input.is_action_pressed("Test1"):
-		get_node("Inventory/Blasters").set_hidden(false)
-		##Applying the rotation of the blasters in radians
-		Offset = -get_viewport().get_canvas_transform().o * get_node("Camera2D").get_zoom() 
-		RelativeMousePos = get_viewport().get_mouse_pos() * get_node("Camera2D").get_zoom() + Offset 
-		get_node("Inventory/Blasters").look_at(RelativeMousePos)
-		#NOT WORKING--Changing scale if on MousePos on -x
-		if RelativeMousePos.x == -RelativeMousePos.x:
-			set_scale(Vector2(-1, 1))
-	else:
-		get_node("Inventory/Blasters").set_hidden(true)
-		set_scale(Vector2(1, 1))
-	if Input.is_action_pressed("Test2"):
-		get_node("Inventory/CursedBlade").set_hidden(false)
-	else:
-		get_node("Inventory/CursedBlade").set_hidden(true)
+	##Weapon Mechanics
+	if WeaponACanEquip && Input.is_action_pressed("Equip"): ##Equip = "E"
+		if WeaponBEquipped: #Checking if another weapon is already equipped and making it invincible
+			get_node("Inventory/CursedBlade").set_hidden(true)
+			get_node("Inventory/Blasters").set_hidden(false)
+			WeaponBEquipped = false
+			WeaponAEquipped = true
+		else:
+			get_node("Inventory/Blasters").set_hidden(false)
+			WeaponAEquipped = true
 		
+	if WeaponBCanEquip && Input.is_action_pressed("Equip"):
+		if WeaponAEquipped:
+			get_node("Inventory/Blasters").set_hidden(true)
+			get_node("Inventory/CursedBlade").set_hidden(false)
+			WeaponAEquipped = false
+			WeaponBEquipped = true
+		else:
+			get_node("Inventory/CursedBlade").set_hidden(false)
+			WeaponBEquipped = true
+			
+	if (WeaponAEquipped or WeaponBEquipped) && Input.is_action_pressed("UnEquip"):##UnEquip = "Q"
+		get_node("Inventory/Blasters").set_hidden(true)
+		get_node("Inventory/CursedBlade").set_hidden(true)
+			
+
+
 	#If no other force applied gravity pushes
 	if JumpAntiForce < Speed * 3:
 		JumpAntiForce += Gravity
@@ -58,19 +69,32 @@ func _fixed_process(delta):
 	else:
 		Velocity = Vector2(0, 0)
 	Velocity.y = JumpAntiForce
-	#If colliding gravity is stopped
+	
 	if is_colliding():
-		JumpAntiForce = 0
-		Gravity = -1
-		Jump = true
-		#get_node("Inventory/Blasters").set_hidden(false)
+		if get_collider().is_in_group("Walkable"): #Checking the type(group) of collision 
+			JumpAntiForce = 0
+			Gravity = 0
+			Jump = true
+		if get_collider().is_in_group("NonWalkable"):
+			Gravity = 0.2
+		if get_collider().is_in_group("Destroyer"):
+			get_node("/root/Main/UI/PopupPanel").popup()
+			queue_free()
+		if get_collider().is_in_group("WeaponCrateA"):
+			WeaponACanEquip = true
+			JumpAntiForce = 0 ##TEMPORARY
+			Gravity = 0 ##TEMPORARY
+		else:
+			WeaponACanEquip = false
+		if get_collider().is_in_group("WeaponCrateB"):###fwew
+			WeaponBCanEquip = true
+			JumpAntiForce = 0 ##TEMPORARY
+			Gravity = 0 ##TEMPORARY
+		else:
+			WeaponBCanEquip = false
 	else:
-		#get_node("Inventory/Blasters").set_hidden(true)
-		Gravity = 0.2
+		Gravity = 0.2  ##Doing the same thing as in the NonWalkable group but ehh
 		
 	#Applying movement
 	Movement = Velocity * (delta + EndSpeed)
 	move(Movement)
-
-func _on_Area2D_area_enter(Area2D):
-	get_node("Inventory/Blasters").set_hidden(false)

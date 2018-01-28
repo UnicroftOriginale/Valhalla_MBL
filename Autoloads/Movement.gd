59e24bc9336a189029af6ac3a724f9eafd29fd60
Neoharry Michailidis <neoharry10@outlook.com.gr>
2018-01-28 17:53:28 +0200

    I dont even no

--- Autoloads/Movement.gd
+++ Autoloads/Movement.gd
@@ -2,10 +2,14 @@ extends Node
 
 var Velocity = Vector2()
 var Movement
+var AntiForce = 0
+var Gravity = 0.2
+var Jump = true
 
 export var Force = 1
 export var Speed = 2
-
+	
+	
 func MoveRight():
 	Velocity.x = Force
 func MoveLeft():
@@ -13,7 +17,18 @@ func MoveLeft():
 func MoveZero():
 	Velocity = Vector2(0, 0)
 func Jump():
-	pass
+	AntiForce += -Speed / 2
+func GravityZero():
+	Gravity = 0
 	
 func _process(delta):
+
+	if AntiForce < Speed / 2:
+		AntiForce += Gravity
+		
+		
+	Velocity.y = AntiForce
+	
 	Movement = Velocity * (delta + Speed)
+	
+	
